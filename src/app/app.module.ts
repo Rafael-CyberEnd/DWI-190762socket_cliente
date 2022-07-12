2f7e1bff3d4fb43527dd1f0a5d3166677a9ba8fa
Rafael Amador Barrios <rafael.amadorbarrios@utxicotepec.edu.mx>
2022-07-12 14:29:37 -0500

    incorporacion grafica de login y rutas de acceso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 const config: SocketIoConfig = 
 {
@@ -13,17 +13,25 @@ import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 import { FooterComponent } from './components/footer/footer.component';
 import { ChatComponent } from './components/chat/chat.component';
+import { ListaUsuariosComponent } from './components/lista-usuarios/lista-usuarios.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MensajesComponent } from './pages/mensajes/mensajes.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    ChatComponent
+    ChatComponent,
+    ListaUsuariosComponent,
+    LoginComponent,
+    MensajesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    SocketIoModule.forRoot(config)
+    SocketIoModule.forRoot(config),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Socket} from 'ngx-socket-io';
+import { Usuario } from '../classes/usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebsocketService {
   public socketStatus=false;
+  public usuario?: Usuario;
 
   constructor(
     private socket: Socket
@@ -31,10 +33,16 @@ export class WebsocketService {
     this.socket.emit(evento, payload, callback);
   }
 
-  listen(evento: string){
-    return this.socket.fromEvent(evento);
+  listen( evento: string){
+    return this.socket.fromEvent( evento );
   }
-
-
+ 
+  loginWS(nombre: String){
+     console.log('Configurando :', nombre)
+     this.socket.emit('configurar-usuario', {nombre}, (resp: Response) =>
+     {
+      console.log(resp);
+     });
+  }
 
 }
